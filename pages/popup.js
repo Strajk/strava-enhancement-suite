391c4abe1079f2bf4b6b7ab215215edf3e9ab893
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-05 20:32:04 +0100

    Switch units.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- pages/popup.js
+++ pages/popup.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('options').setAttribute(
-    'href',
-    chrome.runtime.getURL('pages/options.html')
-  );
+document.getElementById('options').setAttribute(
+  'href',
+  chrome.runtime.getURL('pages/options.html')
+);
+
+document.getElementById('switch_units').addEventListener('click', function() {
+  chrome.tabs.executeScript({
+    file: 'js/switch_units/content_script.js'
+  });
+  window.close();
 });
