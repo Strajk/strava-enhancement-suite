1333ecea0b4b67293b499beedb2269c100a7a2a9
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-06 20:01:43 +0100

    Reflow
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- js/injected.js
+++ js/injected.js
@@ -49,7 +49,11 @@ StravaEnhancementSuite.prototype.default_to_my_results = function() {
     var result = fn.apply(this, Array.prototype.slice.call(arguments));
 
     if (!elem.hasClass('once-only')) {
-      elem.addClass('once-only').find('.clickable[data-filter=my_results]').click();
+      elem
+        .addClass('once-only')
+        .find('.clickable[data-filter=my_results]')
+        .click()
+        ;
     }
 
     return result;
