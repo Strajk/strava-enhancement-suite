5c2b4664c46c6e24e8f1a62c47447ef93a7aa7d3
Chris Lamb <chris@chris-lamb.co.uk>
2014-07-12 13:04:19 +0100

    Abstract for next commit.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- js/injected.js
+++ js/injected.js
@@ -112,12 +112,16 @@ StravaEnhancementSuite.prototype.hide_feed_entries = function() {
 
     jQuery('div.feed>.min-view').each(function() {
       var elem = jQuery(this);
+      var text = elem.text();
 
-      if (that.options.hide_route_feed_entries) {
-        if (elem.text().indexOf('created the route') !== -1) {
-          elem.remove();
+      jQuery.each([
+          ['hide_route_feed_entries', 'created the route']
+        ], function() {
+          if ((that.options[this[0]]) && (text.indexOf(this[1]) !== -1)) {
+            elem.remove();
+          }
         }
-      }
+      );
     });
 
     // Remove any days that are now empty
