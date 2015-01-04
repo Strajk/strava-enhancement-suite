238556cef53affc745265f0e5a47e14419f6c3e0
Chris Lamb <chris@chris-lamb.co.uk>
2015-01-04 17:49:10 +0000

    Move switch units to bottom for next commit
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- js/injected.js
+++ js/injected.js
@@ -700,17 +700,7 @@ StravaEnhancementSuite.prototype.standard_google_map = function() {
   }, 1000);
 };
 
-StravaEnhancementSuite.prototype.switch_units = function() {
-  var url = jQuery("a:contains(My Profile)[href^='/athletes/']").attr('href');
-  var target = window._measurement_preference == "meters" ? "feet" : "meters";
-  var athlete_id = parseInt(url.split('/')[2], 10);
 
-  (new Strava.Athletes.Athlete(url, athlete_id)).save(
-    "measurement_preference",
-    target,
-    {"success": function(x) { window.location.reload(); } }
-  );
-};
 
 StravaEnhancementSuite.prototype.variability_index = function() {
   if (this.options.variability_index === false) {
@@ -809,4 +799,16 @@ StravaEnhancementSuite.prototype.keySort = function() {
 
     return 0;
   };
+};
+
+StravaEnhancementSuite.prototype.switch_units = function() {
+  var url = jQuery("a:contains(My Profile)[href^='/athletes/']").attr('href');
+  var target = window._measurement_preference == "meters" ? "feet" : "meters";
+  var athlete_id = parseInt(url.split('/')[2], 10);
+
+  (new Strava.Athletes.Athlete(url, athlete_id)).save(
+    "measurement_preference",
+    target,
+    {"success": function(x) { window.location.reload(); } }
+  );
 };
