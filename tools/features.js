87c5a5bfe264885d21200b503f01ca2484efc31e
Chris Lamb <chris@chris-lamb.co.uk>
2015-01-12 12:37:05 +0000

    it's called description
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- tools/features.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-eval(fs.readFileSync('pages/options.js') + '');
-
-for (var i = 0; i < StravaEnhancementSuiteOptions.length; ++i) {
-  var option = StravaEnhancementSuiteOptions[i];
-
-  console.log(' - ' + option.title + ': ' + option.description);
-}
