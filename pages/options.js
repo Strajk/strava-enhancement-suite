4bad0d560298808063795c4482915c0ad4a6442a
Chris Lamb <chris@chris-lamb.co.uk>
2015-01-12 04:55:05 +0000

    Render options via JSON
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- pages/options.js
+++ pages/options.js
@@ -1,63 +1,75 @@
-// Requires syncing with js/content_script.js
-DEFAULTS = {
-    annual_achievements: 'show'
-  , comment_post_on_enter: true
-  , convert_units: true
-  , enlarge_on_hover: true
-  , estimated_ftp: true
-  , external_links: true
-  , flyby_select_all: true
-  , hide_blog: false
-  , hide_calories: false
-  , hide_challenge_feed_entries: false
-  , hide_club_feed_entries: false
-  , hide_goal_feed_entries: false
-  , hide_route_feed_entries: false
-  , hide_promotion_feed_entries: false
-  , hide_training_plan_feed_entries: false
-  , hide_shop: false
-  , hide_invite_friends: false
-  , hide_upcoming: false
-  , improve_pagination: true
-  , improve_upload_activity: true
-  , infinite_scroll: true
-  , repeated_segments: true
-  , running_cadence: true
-  , running_heart_rate: true
-  , running_tss: true
-  , side_by_side_running: false
-  , standard_google_map: false
-  , swap_clubs_and_challenge_module: false
-  , variability_index: true
-};
-
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.storage.sync.get(DEFAULTS, function(items) {
-    for (x in DEFAULTS) {
-      var elem = document.getElementById(x);
-
-      elem.value = items[x];
-
-      if ((elem.tagName === 'INPUT') && (elem.getAttribute('type') === 'checkbox')) {
-        elem.checked = items[x];
-      }
+$(function() {
+  $.extend({
+    setOption: function(key, value) {
+      var items = {}
+      items[key] = value;
+      chrome.storage.sync.set(items);
     }
+  });
+
+  $.getJSON('options.json', function(data) {
+    $.each(data, function(idx, option) {
+      var ul = $('<ul/>')
+
+      var elem = $('<input type="checkbox">');
+
+      elem.on('change', function() {
+        $.setOption(option.name, elem.prop('checked'));
+      });
+
+      chrome.storage.sync.get(option.name, function(x) {
+        elem.prop('checked', x[option.name]);
+      });
+
+      if (option.choices) {
+        elem = $('<select>');
+
+        $.each(option.choices, function() {
+          $('<option>')
+            .attr('value', this[0])
+            .text(this[1])
+            .appendTo(elem)
+            ;
+        });
+
+        elem.on('change', function() {
+          $.setOption(option.name, elem.val());
+        });
+
+        chrome.storage.sync.get(option.name, function(x) {
+          elem.val(x[option.name]);
+        });
+      }
+
+      // Add control element
+      $('<li/>')
+        .append(elem)
+        .appendTo(ul)
+        ;
+
+      // Description
+      $('<li class="text"/>')
+        .text(option.description)
+        .appendTo(ul)
+        ;
+
+      // Image
+      if (option.image === true) {
+        $('<li><img></li>')
+          .find('img')
+            .attr('src', 'img/' + option.name + '.png')
+          .end()
+          .appendTo(ul)
+          ;
+      }
+
+      $('<section><h3/></section>')
+        .find('h3')
+          .text(option.title)
+        .end()
+        .append(ul)
+        .appendTo('.content')
+        ;
+    });
   });
 });
-
-for (x in DEFAULTS) {
-  document.getElementById(x).addEventListener('change', function () {
-    var data = {};
-    for (x in DEFAULTS) {
-      var elem = document.getElementById(x);
-
-      data[x] = elem.value;
-
-      if ((elem.tagName === 'INPUT') && (elem.getAttribute('type') === 'checkbox')) {
-        data[x] = elem.checked;
-      }
-    }
-
-    chrome.storage.sync.set(data);
-  });
-}
