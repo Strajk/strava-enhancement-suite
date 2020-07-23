470da99d16f103d53c8319fc9ad0830286091538
Strajk <strajk@me.com>
2020-07-23 09:41:33 +0200

    Notification lib

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -1,10 +1,12 @@
 /* global chrome */
 
-function inject(what) {
+function injectJs(what) {
   return new Promise((resolve, reject) => {
     const el = document.createElement('script');
 
-    if (what.startsWith('/')) {
+    if (what.startsWith('//')) { // Only for local development, CWS disallows remote code
+      el.src = what;
+    } else if (what.startsWith('/')) {
       el.src = chrome.extension.getURL(what);
     } else {
       el.textContent = what;
@@ -17,17 +19,32 @@ function inject(what) {
   });
 }
 
+function injectCss(what) {
+  return new Promise((resolve, reject) => {
+    const el = document.createElement('link');
+    el.href = chrome.extension.getURL(what);
+    el.type = 'text/css';
+    el.rel = 'stylesheet';
+    el.onerror = reject;
+    el.onload = resolve;
+    document.head.appendChild(el);
+  });
+}
+
 chrome.storage.sync.get(null, async (items) => {
-  await inject('/pages/options.js');
+  await injectJs('/pages/options.js');
 
-  await inject('/js/libs/jquery.js'); // jQuery is not present on new, react-only pages, e.g. Training Log
-  await inject('/js/libs/arrive.js');
+  await injectJs('/js/libs/jquery.js'); // jQuery is not present on new, react-only pages, e.g. Training Log
+  await injectJs('/js/libs/arrive.js');
 
-  await inject('/js/libs/react.development.js');
-  await inject('/js/libs/react-dom.development.js');
-  await inject('/js/libs/react-dom-test-utils.development.js');
+  await injectCss('/js/libs/notyf.min.css');
+  await injectJs('/js/libs/notyf.umd.js');
 
-  await inject('/js/main.js');
+  await injectJs('/js/libs/react.development.js');
+  await injectJs('/js/libs/react-dom.development.js');
+  await injectJs('/js/libs/react-dom-test-utils.development.js');
 
-  await inject(`window.strava_enhancement_suite = new StravaEnhancementSuite(jQuery, ${JSON.stringify(items)});`);
+  await injectJs('/js/main.js');
+
+  await injectJs(`window.strava_enhancement_suite = new StravaEnhancementSuite(jQuery, ${JSON.stringify(items)});`);
 });
