973bcd230a352442ca3a75aabfb4976d9715230f
Strajk <strajk@me.com>
2020-07-24 12:46:49 +0200

    Options: Contexts

--- tools/screenshots.js
+++ tools/screenshots.js
@@ -3,6 +3,7 @@ const options = require('./../extension/pages/options.js');
 const MAIN = {
   [options.repeated_segments.name]: () => {
     // https://www.strava.com/activities/3553439073/segments
-
+    // Alpe d'Huez Climb
+    // Fast & Furios Downnill
   },
 };
