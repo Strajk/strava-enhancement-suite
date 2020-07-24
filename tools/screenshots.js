8d46e89d3a1dde051e6fcb115240397c69dfcc54
Strajk <strajk@me.com>
2020-07-24 11:20:02 +0200

    Repeated segments image

---
+++ tools/screenshots.js
@@ -0,0 +1,8 @@
+const options = require('./../extension/pages/options.js');
+
+const MAIN = {
+  [options.repeated_segments.name]: () => {
+    // https://www.strava.com/activities/3553439073/segments
+
+  },
+};
