8429156bcc6dde4d812dbbe71990bb037833d0ff
Thomas Mulder <thomas.mulder@dkv.be>
2021-08-05 00:59:02 +0200

    no need to check all that data if the kudo icon is filled (this check also prevents a bug where the data in data-react-props is not updated after actually giving the kudos, which could result in accidentally opening dozens of popups simultaneously)

--- extension/js/main.js
+++ extension/js/main.js
@@ -1264,6 +1264,7 @@ function StravaEnhancementSuite($, options) {
 
       function checkCanKudo(btn) {
         var $btn = $(btn);
+        if($btn.children("svg").first().attr("data-testid") === "filled_kudos") return false; //no need to check anything further
         var content = $btn.closest(".content");
         var activityData = JSON.parse(content.attr("data-react-props"));
         if (activityData.activity) {
